698ab9260adb616cc29817f25954a49455038e54
LuugIApHAT <luugiaphat2083@gmail.com>
2025-03-30 09:36:34 +0700

    T7

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Luugiaphat.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Luugiaphat.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Model/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Luugiaphat.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Liên kết với Product
+        public List<Product> Products { get; set; }
+    }
+}
